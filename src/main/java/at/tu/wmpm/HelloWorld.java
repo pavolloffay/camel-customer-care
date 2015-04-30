775d10aa7c03656523b82da38f8c6655095a2347
Pavol Loffay <p.loffay@gmail.com>
2015-04-30 21:59:46 +0200

    logging and test dependencies

--- src/main/java/at/tu/wmpm/HelloWorld.java
+++ src/main/java/at/tu/wmpm/HelloWorld.java
@@ -1,11 +1,17 @@
 package at.tu.wmpm;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by pavol on 30.4.2015.
  */
 public class HelloWorld {
 
+    private static Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+
     public static void main(String[] args) {
         System.out.println("HL");
+        logger.debug("debug");
     }
 }
