131a534aa917971965d8b21a82e5c6a5c48f4664
Pavol Loffay <p.loffay@gmail.com>
2015-04-30 21:09:52 +0200

    project template

---
+++ src/main/java/at/tu/wmpm/HelloWorld.java
@@ -0,0 +1,11 @@
+package at.tu.wmpm;
+
+/**
+ * Created by pavol on 30.4.2015.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HL");
+    }
+}
