a709276ac3d30bce8daf483eb163629ddf282082
Johannes Luef <Johannes.Luef@tuwien.ac.at>
2015-05-09 18:25:58 +0200

    Some structure changes, MainApp.java

---
+++ src/main/java/at/tu/wmpm/MainApp.java
@@ -0,0 +1,22 @@
+package at.tu.wmpm;
+
+import org.apache.camel.spring.Main;
+
+
+
+public class MainApp {
+
+    public static void main(String... args) throws Exception {
+        Main main = new Main();
+        main.enableHangupSupport();
+        
+        
+        //adds MailRouter
+        main.addRouteBuilder(new MailRouter());
+        
+        //adds FacebookRouter
+        //main.addRouteBuilder(new FacebookRouter());
+        
+        main.run(args);
+    }
+}
