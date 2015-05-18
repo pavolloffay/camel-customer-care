923487be625823de695b103879c360d19946e610
e1027433 <e1027433@student.tuwien.ac.at>
2015-05-18 21:57:19 +0200

    add mailtranslation in route config and processor for xml translation

--- src/main/java/at/tu/wmpm/RouteConfig.java
+++ src/main/java/at/tu/wmpm/RouteConfig.java
@@ -1,16 +1,17 @@
 package at.tu.wmpm;
 
-import at.tu.wmpm.filter.SpamFilter;
-import at.tu.wmpm.processor.FacebookProcessor;
-import at.tu.wmpm.processor.MailProcessor;
-import at.tu.wmpm.processor.MongoProcessor;
+import javax.annotation.PostConstruct;
+
 import org.apache.camel.builder.RouteBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
-import javax.annotation.PostConstruct;
+import at.tu.wmpm.filter.SpamFilter;
+import at.tu.wmpm.processor.MailToXml;
+import at.tu.wmpm.processor.FacebookProcessor;
+import at.tu.wmpm.processor.MailProcessor;
+import at.tu.wmpm.processor.MongoProcessor;
 
 /**
  * Created by pavol on 30.04.2015 Edited by christian on 07.05.2015
@@ -25,6 +26,8 @@ private static final Logger log = LoggerFactory.getLogger(RouteConfig.class);
     private FacebookProcessor facebookProcessor;
     @Autowired
     private MongoProcessor mongoProcessor;
+    @Autowired
+    private MailToXml mailTranslator;
 
     @PostConstruct
     public void postConstruct() {
@@ -35,6 +38,7 @@ private static final Logger log = LoggerFactory.getLogger(RouteConfig.class);
     public void configure() throws Exception {
 
         from("pop3s://{{eMailUserName}}@{{eMailPOPAddress}}:{{eMailPOPPort}}?password={{eMailPassword}}")
+                .process(mailTranslator)
                 .process(mailProcessor)
                 .choice()
                     .when().method(SpamFilter.class, "isSpam")
---
+++ src/main/java/at/tu/wmpm/processor/MailToXml.java
@@ -0,0 +1,62 @@
+package at.tu.wmpm.processor;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+@Service
+public class MailToXml implements Processor {
+
+	private static final Logger log = LoggerFactory.getLogger(MailToXml.class);
+
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		Message in = exchange.getIn();
+		String inMessageBody = in.getBody(String.class);
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		Document doc = docBuilder.newDocument();
+
+		Element rootElement = doc.createElement("mail");
+		doc.appendChild(rootElement);
+
+		Element sender = doc.createElement("sender");
+		sender.appendChild(doc.createTextNode(in.getHeaders().get("Return-Path").toString()));
+		rootElement.appendChild(sender);
+
+
+		Element subj = doc.createElement("subject");
+        subj.appendChild(doc.createTextNode(in.getHeaders().get("Subject").toString()));
+        rootElement.appendChild(subj);
+
+        Element body = doc.createElement("body");
+        body.appendChild(doc.createTextNode(inMessageBody));
+        rootElement.appendChild(body);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new File(in.getHeaders().get("Subject_").toString()+""+new SimpleDateFormat("ddMMyyyy_HH-mm").format(new Date())+".xml"));
+
+
+        transformer.transform(source, result);
+
+        log.info("XML for mail created");
+	}
+}
