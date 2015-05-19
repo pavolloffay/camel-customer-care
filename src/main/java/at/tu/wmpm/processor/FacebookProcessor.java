ace2413dc21a2326adebedfb7f04573ef3f794da
Fabian Pimminger <fabianpimminger@Fabians-MacBook-Air.local>
2015-05-19 13:09:32 +0200

    facebook processor

--- src/main/java/at/tu/wmpm/processor/FacebookProcessor.java
+++ src/main/java/at/tu/wmpm/processor/FacebookProcessor.java
@@ -1,11 +1,19 @@
 package at.tu.wmpm.processor;
 
+import java.util.Map;
+
 import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.Processor;
+import org.apache.camel.impl.DefaultMessage;
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import facebook4j.Post;
+import at.tu.wmpm.model.FacebookBusinessCase;
+import at.tu.wmpm.model.MailBusinessCase;
 
 /**
  * Created by pavol on 8.5.2015.
@@ -27,8 +35,26 @@ public class FacebookProcessor implements Processor {
 
     @Override
     public void process(Exchange exchange) throws Exception {
+
         log.debug("\n\nFACEBOOK");
         log.debug(ReflectionToStringBuilder.toString(exchange));
         log.debug("\n\n");
+        
+        Message in = exchange.getIn();
+        
+        Post post = (Post)in.getBody();
+        
+        /**
+         * Set new Business case to exchange message
+         */
+        FacebookBusinessCase businessCase = new FacebookBusinessCase();
+        businessCase.setSender(post.getFrom().getName());
+        businessCase.setBody(post.getMessage());
+ 
+        Message message = new DefaultMessage();
+        message.setBody(businessCase);
+        
+        exchange.setOut(message);    	
+
     }
 }
